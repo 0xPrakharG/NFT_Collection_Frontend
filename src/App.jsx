8c9062c01d62f4d84f7c20eb87442d51109cbe76
0xPrakharG <prakhargoyal.3014@gmail.com>
2023-03-23 11:33:55 +0530

    frontend done

---
+++ src/App.jsx
@@ -0,0 +1,76 @@
+import { useEffect, useState } from "react";
+import NFT from "./NFT";
+import { NavLink, Route, Routes } from "react-router-dom";
+import Gallery from "./Gallery";
+import Transfer from "./Transfer";
+import { Web3Button } from "@web3modal/react";
+import { useAccount } from "wagmi";
+
+export default function App() {
+	const {isConnected} = useAccount();
+	return (
+		<div className="App">
+			<nav className="header">
+				<h1>BUIDL_IRL</h1>
+				<div className="Link__Container">
+					<NavLink
+						to="/"
+						className={({ isActive, isPending }) =>
+							`Link${
+								isPending
+									? "--pending"
+									: isActive
+									? "--active"
+									: ""
+							}`
+						}
+					>
+						Create
+					</NavLink>
+					<NavLink
+						to="/gallery"
+						className={({ isActive, isPending }) =>
+							`Link${
+								isPending
+									? "--pending"
+									: isActive
+									? "--active"
+									: ""
+							}`
+						}
+					>
+						Gallery
+					</NavLink>
+				</div>
+			</nav>
+			<div className="container">
+				{isConnected ? (
+					<Routes>
+						<Route path="/" element={<NFT />} />
+						<Route path="gallery/:id?" element={<Gallery />} />
+						<Route path="transfer/:id" element={<Transfer />} />
+					</Routes>
+				) : (
+					<Web3Button />
+				)}
+			</div>
+			<footer>
+				<div className="Logo__Container">
+					<img
+						className="Logo"
+						src="/polygon.svg"
+						alt="Polygon Logo"
+					/>
+					<span>|</span>
+					<img
+						className="Logo"
+						src="/walletconnect.svg"
+						alt="WalletConnect Logo"
+					/>
+				</div>
+			</footer>
+		</div>
+	);
+}
+
+// https://mumbai.polygonscan.com/tx/0x180cedbac58945b77efcfe262b38b9e49b31ae46d99a7f043df49b7bd51ff5b2
---
+++ src/main.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App";
+
+import { EthereumClient, w3mConnectors, w3mProvider } from '@web3modal/ethereum'
+import { Web3Modal } from '@web3modal/react'
+import { configureChains, createClient, WagmiConfig } from 'wagmi'
+import { polygonMumbai } from 'wagmi/chains'
+import "./index.css";
+
+const chains = [polygonMumbai]
+const projectId = "3b2e38ce1e6c6e4a27994d294591a596";
+
+const { provider } = configureChains(chains, [w3mProvider({ projectId })])
+const wagmiClient = createClient({
+  autoConnect: true,
+  connectors: w3mConnectors({ projectId, version: 1, chains }),
+  provider
+})
+const ethereumClient = new EthereumClient(wagmiClient, chains)
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+	<React.StrictMode>
+		<BrowserRouter>
+		<WagmiConfig client={wagmiClient}>
+			<App />
+			<Web3Modal
+        themeVariables={{
+            "--w3m-accent-color": "#8247e5",
+            "--w3m-background-color": "#8247e5",
+        }}
+        projectId={projectId}
+        ethereumClient={ethereumClient}
+    />
+		</WagmiConfig>
+		</BrowserRouter>
+	</React.StrictMode>
+);
